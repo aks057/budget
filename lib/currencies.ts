69083e693cd8105b8def754898d14ad332954e68
Abhinash Kumar <abhinash.iiitl@gmail.com>
2024-07-30 23:42:38 +0530

    pre-final

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -1,7 +1,7 @@
 export const Currencies = [
+  { value: "INR", label: "₹ Rupees", locale: "en-IN" },
   { value: "USD", label: "$ Dollar", locale: "en-US" },
   { value: "EUR", label: "€ Euro", locale: "de-DE" },
-  { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
   { value: "GBP", label: "£ Pound", locale: "en-GB" },
 ];
 
